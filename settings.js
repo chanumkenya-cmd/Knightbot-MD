63dc60921614df48aa7a98228689b9045df60037
chanumkenya-cmd <chanumkenya@gmail.com>
2025-08-26 18:29:17 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'queen b♥️❣️',
   author: 'queen b♥️❣️‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'queen b♥️❣️', // Your name
   ownerNumber: '254742176307', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
